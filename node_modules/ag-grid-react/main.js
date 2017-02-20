c18667972fcb228e787abd492ceedb1b03ac2103
Will Halling <will@Wills-MacBook-Pro.local>
2017-02-20 17:30:41 +0000

    Removed node_modules.

--- node_modules/ag-grid-react/main.js
+++
@@ -1,4 +0,0 @@
-
-exports.AgGridReact = require('./lib/agGridReact').AgGridReact;
-exports.reactCellRendererFactory = require('./lib/reactCellRendererFactory').reactCellRendererFactory;
-exports.reactFilterFactory = require('./lib/reactFilterFactory').reactFilterFactory;
--- node_modules/ag-grid/dist/lib/interfaces/iMenuFactory.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for ag-grid v8.0.1
-// Project: http://www.ag-grid.com/
-// Definitions by: Niall Crosby <https://github.com/ceolter/>
-import { Column } from "../entities/column";
-export interface IMenuFactory {
-    showMenuAfterButtonClick(column: Column, eventSource: HTMLElement): void;
-    showMenuAfterMouseEvent(column: Column, mouseEvent: MouseEvent | Touch): void;
-    isMenuEnabled(column: Column): boolean;
-}
